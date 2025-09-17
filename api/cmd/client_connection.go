5f01ff3e09d2b63cc710f86eae82ccc8cafde494
Nafiz Karim <nafiz.rim@gmail.com>
2025-09-16 22:49:49 -0400

    added handlers for client registration, client login and establishing a web socket

--- api/cmd/client_connection.go
+++ api/cmd/client_connection.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+
+	/*
+		this is mainly for development
+		need to decide if I want this to be within the app struct
+
+	*/
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func (app *application) clientRegisterHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -19,5 +34,15 @@ func (app *application) clientLoginHandler(w http.ResponseWriter, r *http.Reques
 */
 
 func (app *application) clientEstablishWebSocket(w http.ResponseWriter, r *http.Request) {
+	_, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		app.logError(r, fmt.Errorf("could not establish web socket connection %w ", err))
+		return
+	}
+	/*
+		need to add more information about the client in this log
+	*/
+	app.logger.PrintInfo("established webSocket connection with client", nil)
 
 }
